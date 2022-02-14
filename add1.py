cac3e6dbb0aa02d218b0669d88b1306d2e9fab56
rushikesh1110 <99236804+rushikesh1110@users.noreply.github.com>
2022-02-14 14:56:00 +0530

    Create add1.py

---
+++ add1.py
@@ -0,0 +1,6 @@
+num1=int(input("enter first number")
+num2=int(input("enter second number")
+sum=num1+num2
+mul=num1*num2
+print(f"addition of two number "= {sum})
+print (f"multiplication of two number"={mul})
